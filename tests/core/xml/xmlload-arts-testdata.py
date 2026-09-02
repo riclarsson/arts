b689c868c9e02d1a4f6d665ab6ae8d5ab1fd633a
Richard Larsson <ric.larsson@gmail.com>
2026-09-02 10:50:39 +0900

    Fixed subdirs

--- tests/core/xml/xmlload-arts-testdata.py
+++ tests/core/xml/xmlload-arts-testdata.py
@@ -1,9 +1,13 @@
 from xmlload_helper import pyarts, recurse
+import os
 
 path = pyarts.arts.globals.data.arts_source_dir
 if len(path):
-    print(f"arts source found at {path} - starting test run")
-    recurse(path)
+    print(f"arts source found at {path} - starting test run")  
+    subs = ["tests", "examples", "doc", "python", 'src', 'testdata']
+    paths = [os.path.join(path, sub) for sub in subs]
+    for p in paths:
+        recurse(p)
     print("All XML files read successfully!")
 else:
     print("arts source dir not found - skipping test run")
